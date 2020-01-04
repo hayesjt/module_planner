877dd9d91200bb39c595a66a4f9542781ffe8b23
Ashley Wegwerth <ashley.wegwerth@gmail.com>
2020-01-04 10:23:10 -0600

    initial commit

---
+++ planner/Develop/public/js/signup.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  // Getting references to our form and input
+  var signUpForm = $("form.signup");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
+  var nameInput = $("input#name-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", function(event) {
+    event.preventDefault();
+    var userData = {
+      firstName: nameInput.val().trim(),
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim()
+    };
+
+    if (!userData.firstName || !userData.email || !userData.password) {
+      return;
+    }
+    // If we have an email and password, run the signUpUser function
+    signUpUser(userData.firstName, userData.email, userData.password);
+    nameInput.val("");
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(firstName, email, password) {
+    $.post("/api/signup", {
+      firstName: firstName,
+      email: email,
+      password: password
+    })
+      .then(function(data) {
+        window.location.replace("/members");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
---
+++ planner/README.md
@@ -0,0 +1,36 @@
+# Unit 14 Sequelize Homework: Reverse Engineering Code
+
+Reverse engineer the starter code provided and create a tutorial for the code.
+
+In the `Develop` folder, there is starter code for a project. Begin inspecting the code to get an understanding of each file's responsibility. Then, in a Google Doc, write a tutorial explaining *every* file and its purpose. If one file is dependant on other files, be sure to let the user know.
+
+At the end of the tutorial, add instructions for how you could now add changes to this project.
+
+Following the [common templates for user stories](https://en.wikipedia.org/wiki/User_story#Common_templates), we can frame this challenge as follows:
+
+```
+AS A developer
+
+I WANT a walk-through of the codebase
+
+SO THAT I can use it as a starting point for a new project
+```
+
+## Business Context
+
+When joining a new team, you will be expected to inspect a lot of code that you have never seen before. Rather than having a team member explain every line for you, you will dissect the code by yourself, saving any questions for a member of your team.
+
+## Minimum Requirements
+
+* A link to a document or video with instructions on how what each section of code is doing in `Develop/`.
+
+- - -
+
+## Submission on BCS
+
+You are required to submit the following:
+
+* A link to a Google Doc, video, or equivalent that describes the code in `Develop/`. 
+
+- - -
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
